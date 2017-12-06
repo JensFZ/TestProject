6cf6ae59fbf19e1b38f02154bb4a504e631cd3dd
Jens Fischer <JensFZ@gmail.com>
2017-12-06 15:40:17 +0100

    Update install.sh

--- travis/install.sh
+++ travis/install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-echo 'Downloading Unity 5.6.1f1: '
-wget -O Unity.pkg http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorInstaller/Unity-5.6.1f1.pkg
-wget -O Unity-Win.pkg http://netstorage.unity3d.com/unity/2860b30f0b54/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-5.6.1f1.pkg
+echo 'Downloading Unity-2017.2.0f3: '
+wget -O Unity.pkg https://netstorage.unity3d.com/unity/46dda1414e51/MacEditorInstaller/Unity-2017.2.0f3.pkg
+wget -O Unity-Win.pkg http://netstorage.unity3d.com/unity/46dda1414e51/MacEditorTargetInstaller/UnitySetup-Windows-Support-for-Editor-2017.2.0f3.pkg
 echo 'Installing Unity.pkg'
 sudo installer -dumplog -package Unity.pkg -target /
 sudo installer -dumplog -package Unity-Win.pkg -target /
